76ee52ff3e7b6e73c33ddd2376d249f39e501999
Kevin Zhang <19340295+zhangke8@users.noreply.github.com>
2017-09-12 12:32:58 -0400

    Update Crypto.java
    adding in method

--- DEV276x -Java Intro/Crypto.java
+++ DEV276x -Java Intro/Crypto.java
@@ -10,6 +10,20 @@ public class Crypto {
     private static String normalizeText(String text) {
         return text.replaceAll("[^a-zA-Z]", "").toUpperCase();
     }
+    
+    private static String Obfuscation(String text) {
+        String Obfus = "";
+        int i = 0;
+        while (i < text.length()) {
+            String ch = Character.toString(text.charAt(i));
+            if (ch.contains("A") || ch.contains("E") || ch.contains("I") || ch.contains("O") || ch.contains("U")) {
+                Obfus += "OB";
 
+            }
+            Obfus += text.charAt(i);
+            i++;
+        }
+        return Obfus;
+    }
 }
  
